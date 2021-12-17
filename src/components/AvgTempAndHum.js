077457904122bece513dbb9c874a3af0c67dca11
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2021-12-17 12:15:16 +0300

    first commit

---
+++ src/components/AvgTempAndHum.js
@@ -0,0 +1,12 @@
+const AvgTemoAndHum = () => {
+    return(
+        <div className="p-8 bg-blue-100">
+            <div className="bg-white p-6 rounded-lg shadow-lg border-l-3 border-red-500">
+                <h2 className="text-2xl font-semibold mb-2 text-gray-800">Ortalama Nem ve Sıcaklık</h2>
+                <p className="text-gray-500">Nem: 48% - Sıcaklık: 27.2</p>
+            </div>
+        </div>
+    )
+}
+
+export default AvgTemoAndHum
